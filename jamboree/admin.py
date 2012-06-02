38982dcb98ac94553307e7527c6da8445f7ba2cb
Robert Kyle <revspooner@gmail.com>
2012-06-02 18:23:56 +0100

    admin needs initialised

--- jamboree/admin.py
+++ jamboree/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from models import FusionTableExport, FusionTableRowId
+from models import FusionTableExport
 from django import forms
 
 class FusionTableExportAdminForm(forms.ModelForm):
