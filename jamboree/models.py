e44dc2b2781e9b255bb893cce2891a8e70e293ee
Robert Kyle <revspooner@gmail.com>
2012-06-02 16:53:56 +0100

    insert new rows on post_save

--- jamboree/models.py
+++ jamboree/models.py
@@ -1,3 +1,5 @@
+import logging
+
 from django.db import models
 from django.contrib.auth.models import Group
 from django.contrib import admin
@@ -10,7 +12,7 @@ from pyft.fusiontables import FusionTable, DEFAULT_TYPE_HANDLER
 from pyft.fields import NumberField
 from pyft.fields import StringField
 
-# Create your models here.
+log = logging.getLogger(__name__)
 
 # docs groups is a model admin which only lets you create groups from a set form
 
@@ -34,20 +36,20 @@ def get_all_models():
 
 all_models = get_all_models()
 
-class FusionTablesExports(models.Model):
-    django_model = models.ForeignKey(ContentType, null=False, blank=False, editable=False)
-    fusion_table_id = models.CharField(null=True, blank=False, max_length=50, editable=False)
-    fusion_table_url = models.URLField(null=False, blank=False, max_length=500, editable=False)
+class FusionTableExport(models.Model):
+    django_model = models.ForeignKey(ContentType, null=False, blank=False, unique=True, related_name=fusion_table)
+    fusion_table_id = models.CharField(null=True, blank=True, max_length=50)
+    fusion_table_url = models.URLField(null=False, blank=True, max_length=500)
     read_group = models.ForeignKey(Group, null=True, blank=True)
 
-admin.site.register(FusionTablesExports)
+admin.site.register(FusionTableExport)
 
-@receiver(pre_save, sender=FusionTablesExports)
+@receiver(pre_save, sender=FusionTableExport)
 def add_fusion_table(sender, instance, **kwargs):
-    if not instance._entity_exists:
+    if instance.pk is None:
         fusion_table = create_fusion_table(instance)
         instance.fusion_table_id = fusion_table.table_id
-        instance.fusion_table_url = "https://www.google.com/fusiontables/DataSource?docid=" + fusion_table.table_id
+        instance.fusion_table_url = "https://www.google.com/fusiontables/DataSource?dsrcid=%s" %fusion_table.table_id
 
 def create_fusion_table(instance):
     fusion_table_name = "%s - %s" %(instance.django_model.app_label, instance.django_model.name)
@@ -57,7 +59,7 @@ def create_fusion_table(instance):
     return fusion_table
 
 DJANGO_TO_FUSION_TABLES_FIELD_TYPE_MAP = {
-    'django.db.models.fields.CharField': StringField.column_type
+    'django.db.models.fields.CharField': StringField
 }
 
 def get_schema_from_model(django_model):
@@ -67,8 +69,39 @@ def get_schema_from_model(django_model):
     for field_name in field_names:
         field_type = django_model_class._meta.get_field_by_name('id')[0].__class__
         if field_type in DJANGO_TO_FUSION_TABLES_FIELD_TYPE_MAP:
-            schema[field_name] = DJANGO_TO_FUSION_TABLES_FIELD_TYPE_MAP[field_type]
+            schema[field_name] = DJANGO_TO_FUSION_TABLES_FIELD_TYPE_MAP[field_type].column_type
         else:
             schema[field_name] = StringField.column_type
 
     return schema
+
+@receiver(post_save)
+def fusion_model_change(sender, instance, created, **kwargs):
+    sender_type = ContentType.objects.get_for_model(sender)
+        try:
+            sender_fusion_table = sender_type.fusion_table_set.exclude(fusion_table_id=None).get()
+            if created is True:
+                insert_rows(sender_fusion_table.table_id, [instance])
+            else:
+                pass #update_row()
+        except FusionTableExport.DoesNotExist:
+            pass
+
+def insert_rows(table_id, instances):
+    fusion_table = FusionTable(table_id)
+    rows = []
+    for instance in instances:
+        fusion_table_fields = build_fields_for_row(instance)
+        rows.append(Row(rowid=None, fields=fusion_table_fields))
+
+    fusion_table.insert(rows)
+    
+
+def build_fields_for_row(instance):
+    django_model = instance.__class__
+    field_names = django_model._meta.get_all_field_names()
+    schema = get_schema_from_model(django_model)
+    fusion_table_fields = []
+    for field_name in field_names:
+        fusion_table_fields.append(schema[field_name](instance.getattr(field_name), column_name=field_name))
+    return fusion_table_fields
