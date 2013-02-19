e0542894a69698c06d924d562658a9c192cd0afc
ec2_server <revspooner@gmail.com>
2013-02-19 17:25:47 +0000

    add missing import

--- djangofusiontables/signals.py
+++ djangofusiontables/signals.py
@@ -1,3 +1,5 @@
+import logging
+
 from django.db.models.signals import pre_save, post_save, pre_delete
 from django.dispatch import receiver
 from django.contrib.contenttypes.models import ContentType
